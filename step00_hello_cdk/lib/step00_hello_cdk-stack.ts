46a826f08dd140280efbd6c66b974eabab71bbd0
Mohammad Haseeb <mohammad.haseeb310@gmail.com>
2022-11-02 20:50:14 +0500

    sr configured  with aws-cdk

---
+++ step00_hello_cdk/lib/step00_hello_cdk-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Step00HelloCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Step00HelloCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    new cdk.aws_s3.Bucket(this,"helloBucket",{
+      versioned:true
+    })
+  }
+}
